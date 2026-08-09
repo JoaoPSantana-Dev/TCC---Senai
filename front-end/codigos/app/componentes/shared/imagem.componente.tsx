81b3bbbc3bd6dd929589c2517bc36672c607f5bf
thalles-casa <thazez3390@gmail.com>
2026-08-09 10:05:12 -0300

    agora as imagens são dinâmicas, o bloco da imagem cobre a maior parte e some ao ficar com a tela pequena e a fonte agora é montserrat

--- front-end/codigos/app/componentes/shared/imagem.componente.tsx
+++ front-end/codigos/app/componentes/shared/imagem.componente.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 
-export default function Imagem() {
+interface ImagemProps {
+  nomeImagem: string;
+  alt?: string;
+}
+
+export default function Imagem({ nomeImagem, alt = "Imagem" }: ImagemProps) {
   return (
     <Image
-      src="/senai-mariano-ferraz.png"
-      alt="Logo SENAI Mariano Ferraz"
+      src={`/${nomeImagem}`}
+      alt={alt}
       width={1920}
       height={1080}
       className="w-full h-full object-cover"
+      loading="eager"
     />
   );
 }
--- front-end/codigos/app/componentes/shared/login.componente.tsx
+++ front-end/codigos/app/componentes/shared/login.componente.tsx
@@ -4,14 +4,17 @@ import Imagem from "./imagem.componente";
 
 export function Login() {
   return (
-    <main className="flex flex-col md:flex-row min-h-screen w-full gap-x-10">
-      <aside className="w-full md:w-1/2 bg-white flex items-center justify-center">
-        <Imagem />
+    <main className="h-screen w-full overflow-hidden flex flex-col md:flex-row gap-x-10">
+      <aside className="hidden md:flex md:w-2/3 bg-white items-center justify-center">
+        <Imagem
+          nomeImagem="senai-mariano-ferraz.png"
+          alt="Foto da entrada da escola SENAI Mariano Ferraz"
+        />
       </aside>
 
-      <div className="w-full md:w-1/2 bg-white flex flex-col justify-center p-8 md:p-16 gap-x-10">
-        <header className="flex flex-col gap-2 mb-6">
-          <h1 className="text-2xl font-bold">Login</h1>
+      <div className="w-full h-full md:w-1/3 bg-white flex flex-col justify-center p-8 md:p-16 gap-x-10">
+        <header className="flex flex-col gap-2 mb-10">
+          <h1 className="text-lg font-bold">Login</h1>
           <p className="text-gray-600">
             Entre usando seu CPF e senha cadastrados no sistema
           </p>
@@ -27,7 +30,7 @@ export function Login() {
               maxLength={14}
               inputMode="numeric"
               required
-              className="border p-2 rounded"
+              className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
             />
           </div>
 
@@ -38,13 +41,13 @@ export function Login() {
               name="senha"
               placeholder="Insira sua senha"
               required
-              className="border p-2 rounded"
+              className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
             />
           </div>
 
           <button
             type="submit"
-            className="bg-blue-600 hover:bg-blue-700 text-white py-2 rounded mt-2"
+            className="bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-3xl mt-2 duration-150"
           >
             Entrar
           </button>
