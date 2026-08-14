add6a024f57f8aaa465a7a45580161fe6b43ceee
thalles-b09 <thalles.d@aluno.senai.br>
2026-08-14 16:31:10 -0300

    header com padrão de botão menu (ainda falta estilização)

--- front-end/codigos/app/components/totem/homepage/header.component.tsx
+++ front-end/codigos/app/components/totem/homepage/header.component.tsx
@@ -22,7 +22,7 @@ export function HeaderComponent() {
             </span>
           </div>
 
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center bg-white p-4">
             <figure className="flex justify-center">
               <ImagemComponent
                 nomeImagem="senai-logo.png"
@@ -40,9 +40,31 @@ export function HeaderComponent() {
   // esse código é para todas as outras páginas que usarem o header e que não sejam o "/homepage"
   return (
     <>
-      <header className="w-full text-white flex flex-row bg-red-600">
-        <Link href="/homepage">MENU</Link>
-        <h1>aaaa</h1>
+      <header className="w-full text-white flex flex-col">
+        <div className="flex flex-row w-screen bg-red-600 pl-2 gap-2 justify-between pr-2">
+          <span className="flex">
+            <h1>FIESP CIESP SESI SENAI IRS | F | TW | YT | iN | iNt | wz</h1>
+          </span>
+
+          <span className="flex items-end justify-end">
+            <h1>16:00 21°</h1>
+          </span>
+        </div>
+
+        <div className="flex flex-col items-center bg-white p-4">
+          <nav>
+            <Link href="/homepage">MENU</Link>
+          </nav>
+
+          <figure className="flex justify-center">
+            <ImagemComponent
+              nomeImagem="senai-logo.png"
+              alt="Imagem do logo SENAI"
+              width={200}
+              height={200}
+            />
+          </figure>
+        </div>
       </header>
     </>
   );
