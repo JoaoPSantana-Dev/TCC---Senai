4756e7f02c2740fdf1effb963d9e6a4b2d5df3af
thalles-delete <thazez3390@gmail.com>
2026-08-14 20:54:34 -0300

    adicionado o footer que o joão fez e feito algumas mudanças

--- front-end/codigos/app/components/administrador/login.component.tsx
+++ front-end/codigos/app/components/administrador/login.component.tsx
@@ -3,55 +3,62 @@
 import BotaoLogin from "./botao-login.component";
 import ImagemComponent from "./imagem.component";
 import { ImagemLoginComponent } from "./imagem-login.component";
+import FooterComponent from "../totem/shared/footer.component";
 
 export function LoginComponent() {
   return (
-    <main className="h-screen w-full overflow-hidden flex flex-col md:flex-row">
-      <ImagemLoginComponent />
+    <div className="min-h-screen flex flex-col">
+      <main className="flex flex-1 flex-col md:flex-row">
+        <ImagemLoginComponent />
 
-      <section className="w-full h-full md:w-1/3 bg-white flex flex-col justify-between p-8 md:p-16">
-        <header className="flex flex-col gap-2">
-          <figure className="mb-32">
-            <ImagemComponent
-              nomeImagem="senai-logo.png"
-              alt="Imagem do logo SENAI"
-            />
-          </figure>
+        <section className="w-full h-full md:w-1/3 bg-white flex flex-col justify-between p-8 md:p-16">
+          <header className="flex flex-col gap-2">
+            <figure className="mb-32">
+              <ImagemComponent
+                nomeImagem="senai-logo.png"
+                alt="Imagem do logo SENAI"
+              />
+            </figure>
 
-          <h1 className="text-lg font-bold">Login</h1>
-          <p className="text-gray-600">
-            Entre usando seu CPF e senha cadastrados no sistema
-          </p>
-        </header>
+            <h1 className="text-lg font-bold">Login</h1>
+            <p className="text-gray-600">
+              Entre usando seu CPF e senha cadastrados no sistema
+            </p>
+          </header>
 
-        <form className="flex flex-col gap-4 mb-auto pt-6">
-          <div className="flex flex-col gap-1">
-            <label htmlFor="cpf">CPF</label>
-            <input
-              type="text"
-              name="cpf"
-              placeholder="___.___.___-__"
-              maxLength={14}
-              inputMode="numeric"
-              required
-              className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
-            />
-          </div>
+          <form className="flex flex-col gap-4 mb-auto pt-6">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="cpf">CPF</label>
+              <input
+                type="text"
+                name="cpf"
+                placeholder="___.___.___-__"
+                maxLength={14}
+                inputMode="numeric"
+                required
+                className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
+              />
+            </div>
 
-          <div className="flex flex-col gap-1">
-            <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              name="senha"
-              placeholder="Insira sua senha"
-              required
-              className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
-            />
-          </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="senha">Senha</label>
+              <input
+                type="password"
+                name="senha"
+                placeholder="Insira sua senha"
+                required
+                className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
+              />
+            </div>
 
-          <BotaoLogin nomeImagem="botao_login.svg" />
-        </form>
-      </section>
-    </main>
+            <BotaoLogin nomeImagem="botao_login.svg" />
+          </form>
+        </section>
+      </main>
+
+      <div className="mt-auto">
+        <FooterComponent />
+      </div>
+    </div>
   );
 }
---
+++ front-end/codigos/app/components/totem/shared/footer.component.tsx
@@ -0,0 +1,18 @@
+export default function FooterComponent() {
+  return (
+    <footer className="flex bg-white text-black py-2 text-center border-t-2 border-gray-300 pb-4">
+      <div className="container mx-auto px-4 flex flex-col items-center gap-4">
+        <p className="text-sm font-medium">© 2026 Sector</p>
+        <div>
+          <ul className="flex flex-wrap justify-center gap-x-6 gap-y-2 text-xs opacity-90">
+            <li className="hover:no-underline cursor-pointer">Eduardo</li>
+            <li className="hover:no-underline cursor-pointer">João Paulo</li>
+            <li className="hover:no-underline cursor-pointer">João Pedro</li>
+            <li className="hover:no-underline cursor-pointer">Thalles</li>
+            <li className="hover:no-underline cursor-pointer">Tomás</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
