25a50813a0ea12a85ef71a3c552318c540d36673
João Pedro Senai-B09 <joao.p.silva662@aluno.senai.br>
2026-08-21 14:53:09 -0300

    Finalização da pagina de 'descanso'

--- front-end/codigos/app/components/shared/descanso.component.tsx
+++ front-end/codigos/app/components/shared/descanso.component.tsx
@@ -5,7 +5,8 @@ import ImagemLogoComponent from "./imagem.logo.component";
 
 export default function TelaDescanso() {
   return (
-    <main className="flex h-screen w-screen items-center justify-center bg-[#EAEAEA]">
+    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-[#EAEAEA]">
+      <h1 className="mb-8 text-2xl md:text-3xl font-bold text-zinc-800 text-center">Bem vido ao SENAI Mariano Ferraz!</h1>
       <div className="flex min-h-400px w-400px flex-col items-center justify-center rounded-2xl bg-white p-8 shadow-lg">
         <figure className="mb-12">
           <ImagemLogoComponent nomeImagem="senai-logo.png" alt="Imagem do logo SENAI" />
