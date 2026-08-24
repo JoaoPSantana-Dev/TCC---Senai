197b045546f66e0325d3409e82b32ed99d5d7ed8
joao <joao@gmail.com>
2026-08-24 13:36:34 -0300

    Update app.module.ts

--- BackEnd/db/src/app.module.ts
+++ BackEnd/db/src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { DadosModule } from './dados/usuarios/dados.module';
+import { HorariosAulaModule } from './dados/horarios-aula/horarios-aula.module';
+import { PaginasModule } from './dados/paginas/paginas.module';
+import { SalasModule } from './dados/salas/salas.module';
+import { TextosModule } from './dados/textos/textos.module';
+import { VagasEmpregoModule } from './dados/vagas-emprego/vagas-emprego.module';
+import { VagasEstagioModule } from './dados/vagas-estagio/vagas-estagio.module';
+import { UsuariosModule} from './dados/usuarios/usuarios.module';
 
 @Module({
-  imports: [PrismaModule, DadosModule],
-  
-  
+  imports: [
+    PrismaModule,
+    HorariosAulaModule,
+    PaginasModule,
+    SalasModule,
+    TextosModule,
+    VagasEmpregoModule,
+    VagasEstagioModule,
+    UsuariosModule,
+  ],
 })
 
 export class AppModule {}
