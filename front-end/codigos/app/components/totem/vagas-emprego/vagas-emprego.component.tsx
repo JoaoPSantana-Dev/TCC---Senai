f26c01b0026cfc430a6d43247287fc9103536ff7
thalles-delete <thazez3390@gmail.com>
2026-08-29 20:52:32 -0300

    agora a tag section com o h1 virou um componente e a homepage e vagas-emprego agora usam elas

--- front-end/codigos/app/components/totem/vagas-emprego/vagas-emprego.component.tsx
+++ front-end/codigos/app/components/totem/vagas-emprego/vagas-emprego.component.tsx
@@ -1,12 +1,10 @@
+import { TituloComponent } from "../shared/titulo.component";
 import { VagasEmpregoMenuComponent } from "./vagas-emprego-menu.component";
 
 export function VagasEmpregoComponent() {
   return (
-    <section className="min-h-auto flex flex-col items-center justify-center">
-      <h1 className="text-center text-2xl font-semibold my-2 pb-8">
-        Oportunidades de emprego
-      </h1>
+    <TituloComponent texto="Oportunidades de emprego">
       <VagasEmpregoMenuComponent />
-    </section>
+    </TituloComponent>
   );
 }
