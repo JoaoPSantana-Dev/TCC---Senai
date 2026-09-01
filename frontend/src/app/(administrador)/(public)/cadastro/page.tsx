01000d51d66592da53b80af68565de7a8b2d7979
eduardo <es904188@gmail.com>
2026-09-01 16:03:16 -0300

    arrumando front-end

--- frontend/src/app/(administrador)/(public)/cadastro/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CadastroComponent } from "@/app/components/administrador/cadastro/cadastro.component";
-
-export default function Cadastro() {
-  return (
-    <div className="flex h-screen w-full overflow-hidden min-h-screen flex-col">
-      <CadastroComponent />
-    </div>
-  );
-}
