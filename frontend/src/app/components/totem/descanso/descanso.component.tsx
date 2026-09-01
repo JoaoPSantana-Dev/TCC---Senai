df72f20957ab9a3cdfaad2f163cbd14c576425dd
joao <joao@gmail.com>
2026-09-01 16:40:58 -0300

    Atualizacao de branch

--- frontend/src/app/components/totem/descanso/descanso.component.tsx
+++ frontend/src/app/components/totem/descanso/descanso.component.tsx
@@ -19,6 +19,7 @@ export default function TelaDescanso() {
         <ImagemBotaoComponent
           nomeImagem="botao_login.svg"
           texto="Click para começar"
+          
         />
       </div>
     </main>
