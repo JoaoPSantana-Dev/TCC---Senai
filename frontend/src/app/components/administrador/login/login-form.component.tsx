db0119a56ab923c0f7fe51ab517eb2c518c9a050
joao <joao@gmail.com>
2026-09-02 14:34:38 -0300

    Uso de zod no login

--- frontend/src/app/components/administrador/login/login-form.component.tsx
+++ frontend/src/app/components/administrador/login/login-form.component.tsx
@@ -1,78 +1,95 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
 import { ImagemBotaoComponent } from "../../shared/imagem-botao.component";
-import { useState } from "react";
+
+const schemaLogin = z.object({
+  email: z.email("Endereço de email invalido"),
+  senha: z.string().min(8, "Senha precisa ter no mínimo 8 caracteres"),
+});
+
+type LoginFormData = z.infer<typeof schemaLogin>;
+
 
 export function LoginFormComponent() {
   const router = useRouter();
-  const [loading, setLoading] = useState(false);
+  
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: {errors, isSubmitting},
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(schemaLogin),
+    defaultValues:{
+      email: "",
+      senha: "",
+    },
+  });
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    setLoading(true);
-
-    const form = event.currentTarget;
-    const email = form.email.value;
-    const senha = form.senha.value;
-
-    try {
+  const onSubmit = async(data: LoginFormData)=>{
+     try {
       const resposta = await fetch("http://localhost:3001/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          email,
-          senha,
-        }),
+        body: JSON.stringify(data),
       });
 
       const dados = await resposta.json();
 
-      if (resposta.ok) {
-        if (dados.token) {
-          localStorage.setItem("token", dados.token);
-        }
+      if(resposta.ok){
         router.push("/homepage");
         return;
       }
-
-      form.senha.value = "";
-      alert(dados.message || "Credenciais inválidas");
-    } catch (error) {
-      alert("Erro ao tentar realizar login");
-    } finally {
-      setLoading(false);
-    }
+      else{
+        alert(dados.message || "Erro ao realizar login");
+        reset();
+      } 
   }
+  catch(erro){
+    alert("Falha ao conectar com o servidor");
+  }
+}
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4 pt-6">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 pt-6">
       {/* campo para inserir o Email do usuário. */}
       <div className="flex flex-col gap-1 ">
         <label htmlFor="email">Email</label>
         <input
+          id="email"
           type="email"
-          name="email"
-          placeholder="Digite seu email"
+          {...register("email")}
           //maxLength={14}
           //inputMode="numeric"
           required
           className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
+          placeholder="Digite seu email"
         />
+        {errors.email && (
+          <p className="text-red-500 text-sm">{errors.email.message}"</p>
+        )}
       </div>
 
       {/* campo para inserir a senha do usuário. */}
       <div className="flex flex-col gap-1">
         <label htmlFor="senha">Senha</label>
         <input
+          id="senha"
           type="password"
-          name="senha"
-          placeholder="Insira sua senha"
+          {...register("senha")}
           required
           className="border border-zinc-400 hover:border-zinc-800 p-4 rounded-2xl"
+          placeholder="Insira sua senha"
         />
+        {errors.senha && (
+          <p className="text-red-500 text-sm">{errors.senha.message}</p>
+        )}
       </div>
 
       {/* botão de envio do formulário de autenticação. */}
@@ -81,4 +98,5 @@ export function LoginFormComponent() {
       </div>
     </form>
   );
+
 }
