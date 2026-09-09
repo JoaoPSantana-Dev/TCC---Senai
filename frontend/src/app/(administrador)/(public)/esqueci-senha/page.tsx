3ac7681a34ff5ecc521af93a1b7c289a0d0300a7
thalles-b10 <thalles.d@aluno.senai.br>
2026-09-09 16:52:02 -0300

    iniciação da página de esqueci a senha, ainda falta alguns fru fru e funcionalidades

---
+++ frontend/src/app/(administrador)/(public)/esqueci-senha/page.tsx
@@ -0,0 +1,9 @@
+import { EsqueciSenhaComponent } from "@/app/components/administrador/esqueci-senha/esqueci-senha.component";
+
+export default function Cadastro() {
+  return (
+    <div className="flex h-screen w-full overflow-hidden min-h-screen flex-col">
+      <EsqueciSenhaComponent />
+    </div>
+  );
+}
