afebd5cdfefc5080e0edc429e14bef328da099bb
thalles-b09 <thalles.d@aluno.senai.br>
2026-09-18 16:41:35 -0300

    tentando criar página de criar páginas

--- frontend/src/app/(administrador)/(private)/paginas/page.tsx
+++ frontend/src/app/(administrador)/(private)/paginas/page.tsx
@@ -3,13 +3,15 @@ import { Imagem } from "@/app/components/shared/Imagem";
 import { User02Icon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 
-export default function Usuarios() {
+export default function Paginas() {
   return (
     <div className="w-screen h-screen flex flex-col">
       <header className="w-full flex flex-row bg-white justify-between py-2 border-b-2 border-gray-300 mb-8">
         <NavbarHamburguer className="absolute w-18 h-10 left-6 top-10.5 -translate-y-1/2" />
 
-        <Imagem nomeImagem="senai-logo.png" width={200} height={50} />
+        <figure className="flex justify-center">
+          <Imagem nomeImagem="senai-logo.png" width={200} height={50} />
+        </figure>
 
         <div className="flex pr-10 items-center">
           <HugeiconsIcon
@@ -21,9 +23,21 @@ export default function Usuarios() {
         </div>
       </header>
 
-      <main>
-        <h1>asdawd</h1>
-        <p>asdalwh</p>
+      <main className="flex justify-center items-center w-full">
+        <section className="grid grid-cols-3 bg-white w-1/3 md:w-2/3">
+          <div className="flex flex-col justify-center">
+            <h1>Nome</h1>
+            <p>asdalwh</p>
+          </div>
+          <div className="flex flex-col justify-center">
+            <h1>Tipo</h1>
+            <p>asdalwh</p>
+          </div>
+          <div className="flex flex-col justify-center">
+            <h1>Opções</h1>
+            <p>asdalwh</p>
+          </div>
+        </section>
       </main>
     </div>
   );
--- frontend/src/app/components/shared/NavbarHamburguer.tsx
+++ frontend/src/app/components/shared/NavbarHamburguer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { slide as Menu } from "react-burger-menu";
 
